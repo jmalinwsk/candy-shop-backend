60bd8444c85b0dff84a18a02d5494eb905db7d02
crin <crinuyi@gmail.com>
2024-02-06 15:05:51 +0100

    implemented changing password via sent email

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ const validateMongoDBID = require("../utils/validateMongoDBID");
 const { generateRefreshToken } = require("../configs/refreshToken");
 const jwt = require("jsonwebtoken");
 const sendEmail = require("./emailController");
+const crypto = require("crypto");
 
 const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
@@ -196,6 +197,22 @@ const forgotPasswordToken = asyncHandler(async (req, res) => {
     throw new Error(err);
   }
 });
+const resetPassword = asyncHandler(async (req, res) => {
+  const {password} = req.body;
+  const { token } = req.params;
+  const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+  const user = await User.findOne({
+    passwordResetToken:  hashedToken,
+    passwordResetExpires: {$gt: Date.now()}
+  });
+  if(!user)
+    throw new Error("Token expired!");
+  user.password = password;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+  res.json(user);
+})
 
 module.exports = {
   createUser,
@@ -210,4 +227,5 @@ module.exports = {
   handleRefreshToken,
   updatePassword,
   forgotPasswordToken,
+  resetPassword
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -12,6 +12,7 @@ const {
   logoutUser,
   updatePassword,
   forgotPasswordToken,
+  resetPassword
 } = require("../controllers/userController");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
@@ -19,6 +20,7 @@ const router = express.Router();
 router.post("/register", createUser);
 router.put("/password", authMiddleware, updatePassword);
 router.post("/forgot-password", forgotPasswordToken);
+router.put("/reset-password/:token", resetPassword);
 router.post("/login", loginUser);
 router.get("/all-users", getUsers);
 router.get("/refresh", handleRefreshToken);
