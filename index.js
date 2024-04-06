e123aa47c2ae041cd85516f6d6a0fc15a540c58e
crin <crinuyi@gmail.com>
2024-04-06 04:42:06 +0200

    enabled CORS

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ const bodyParser = require("body-parser");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
+const cors = require("cors");
 
 databaseConnect();
 app.use(morgan("dev"));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
